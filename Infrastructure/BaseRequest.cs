4de75ad8972a2c3b42baa6f801f1d8aafad42849
PC <PC@DESKTOP-T7PJKVM>
2025-09-16 17:43:56 +0700

    init

---
+++ Infrastructure/BaseRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure
+{
+    public abstract class BaseMessage
+    {
+        /// <summary>
+        /// Unique Identifier used by logging
+        /// </summary>
+        protected Guid _correlationId = Guid.NewGuid();
+        public Guid CorrelationId() => _correlationId;
+    }
+    public class BaseRequest: BaseMessage
+    {
+    }
+}
