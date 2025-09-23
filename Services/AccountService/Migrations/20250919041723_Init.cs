226eed481c8eaa84465dae448aa60a4a31022691
PC <PC@DESKTOP-T7PJKVM>
2025-09-23 17:41:20 +0700

    init

---
+++ Services/AccountService/Migrations/20250919041723_Init.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "minhdc223@gmail.com");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Email",
+                value: "");
+        }
+    }
+}
